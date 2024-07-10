bd91c46ff2526b26f8c1d0a6c4b17f7c875a62fe
Staid123 <pavlenkokirill120@gmail.com>
2024-07-10 17:56:04 +0300

    Update models soon

---
+++ booking-system/authentication_service/auth/models.py
@@ -0,0 +1,45 @@
+from datetime import (
+    datetime,
+    timedelta
+)
+
+from typing import List
+
+from sqlalchemy import (
+    String,
+    Integer,
+    ForeignKey,
+    DateTime
+)
+from sqlalchemy.orm import (
+    Mapped,
+    DeclarativeBase,
+    mapped_column,
+    relationship
+
+)
+
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str]
+    password_hash: Mapped[str]
+    sessions: Mapped[List["Session"]] = relationship(back_populates="user", uselist=True)
+
+
+class Session(Base):
+    __tablename__ = "session"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[str] = mapped_column(ForeignKey("user.id"))
+    token: Mapped[str] = mapped_column(String, nullable=False)
+    expires_at: Mapped[datetime] = mapped_column(DateTime, default=get_default_expires_at)
+    user: Mapped["User"] = relationship(back_populates="sessions", uselist=False)
