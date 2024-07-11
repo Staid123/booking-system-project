5ac0c852b86f2da6403ece23120d1cb53a3e2e0e
Staid123 <pavlenkokirill120@gmail.com>
2024-07-11 17:53:31 +0300

    new exceptions

--- booking-system/authentication_service/auth/custom_exceptions.py
+++ booking-system/authentication_service/auth/custom_exceptions.py
@@ -1,4 +1,18 @@
+from fastapi import HTTPException, status
+
+
 class UserCreateException(Exception):
     def __init__(self, message="Failed to create user"):
         self.message = message
         super().__init__(self.message)
+
+
+unauthed_user_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="invalid username or password",
+)
+
+unactive_user_exception = HTTPException(
+    status_code=status.HTTP_403_FORBIDDEN,
+    detail="user inactive",
+)
