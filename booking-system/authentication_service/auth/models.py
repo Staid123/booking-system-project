c447e9701ce3e4e5347ef205c592cef45ecffa7d
Staid123 <pavlenkokirill120@gmail.com>
2024-07-12 16:27:12 +0300

    Deleted session table

--- booking-system/authentication_service/auth/models.py
+++ booking-system/authentication_service/auth/models.py
@@ -1,16 +1,7 @@
-from datetime import datetime
-
-from sqlalchemy import (
-    String,
-    Integer,
-    ForeignKey,
-    DateTime
-)
 from sqlalchemy.orm import (
     Mapped,
     DeclarativeBase,
     mapped_column,
-    relationship
 )
 
 
@@ -26,14 +17,3 @@ class User(Base):
     username: Mapped[str]
     email: Mapped[str]
     password_hash: Mapped[str]
-    sessions: Mapped[list["Session"]] = relationship(back_populates="user", uselist=True)
-
-
-class Session(Base):
-    __tablename__ = "session"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[str] = mapped_column(ForeignKey("user.id"))
-    token: Mapped[str]
-    expires_at: Mapped[datetime]
-    user: Mapped["User"] = relationship(back_populates="sessions", uselist=False)
