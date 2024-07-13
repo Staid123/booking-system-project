deabe5259c5a2022f544d7cfd25fe0cf31d50b26
Kirill <pavlenkokirill120@gmail.com>
2024-07-13 12:42:58 +0300

    User service for interaction with repository

---
+++ booking-system/authentication_service/service/user_service.py
@@ -0,0 +1,73 @@
+from abc import ABC, abstractmethod
+from typing import Annotated
+
+from fastapi import Depends
+
+from auth.models import User
+from repository.user_repository import UserRepository, get_user_repository
+from sqlalchemy.orm import Session
+from auth.schemas import UserIn, UserOut
+from auth.custom_exceptions import UserCreateException
+
+
+class AbstractUserService(ABC):
+    @staticmethod
+    @abstractmethod
+    def register_user():
+        raise NotImplementedError
+    
+    @staticmethod
+    @abstractmethod
+    def get_user_by_email():
+        raise NotImplementedError
+    
+    @staticmethod
+    @abstractmethod
+    def list_users():
+        raise NotImplementedError
+
+
+class UserService(AbstractUserService):
+    @staticmethod
+    def register_user(
+        session: Session, 
+        user_in: UserIn,
+        user_repository: UserRepository = get_user_repository()
+    ) -> int:
+        try:
+            new_user_id = user_repository.create_user(
+                session=session,
+                **user_in.model_dump()
+            )
+            return new_user_id
+        except UserCreateException as ex:
+            return f"{ex}: failure to create new user"
+
+    @staticmethod
+    def get_user_by_email(
+        session: Session, 
+        email: str,
+        user_repository: UserRepository = get_user_repository(),
+    ) -> UserIn:
+        user: User | None = user_repository.get_user_by_email(
+            session=session, 
+            email=email
+        )
+        if user:
+            return UserOut.model_validate(obj=user, from_attributes=True)
+        return None
+    
+
+    @staticmethod
+    def list_users(
+        user_repository: Annotated[UserRepository, Depends(get_user_repository)],
+        session: Session
+    ) -> list[UserOut]:
+        users = user_repository.get_all_users(session=session)
+        users_schemas = [UserOut.model_validate(user) for user in users]
+        return users_schemas
+    
+
+# Зависимость для получения сервиса
+def get_user_service():
+    return UserService
