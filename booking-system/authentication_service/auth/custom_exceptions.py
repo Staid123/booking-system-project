c10166577a4c8b11ef3c9805d18af7d78a67daa6
Kirill <pavlenkokirill120@gmail.com>
2024-07-14 13:45:12 +0300

    new exceptions

--- booking-system/authentication_service/auth/custom_exceptions.py
+++ booking-system/authentication_service/auth/custom_exceptions.py
@@ -3,9 +3,9 @@ from jwt import InvalidTokenError
 
 
 class UserCreateException(Exception):
-    def __init__(self, message="Failed to create user"):
+    def init(self, message="Failed to create user"):
         self.message = message
-        super().__init__(self.message)
+        super().init(self.message)
 
 
 unauthed_user_exception = HTTPException(
@@ -37,7 +37,26 @@ invalid_token_type_exception = HTTPException(
     detail=f"invalid token type"
 )
 
+
 not_enough_rights_exception = HTTPException(
     status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-    detail=f"have not enough rights exception"
+    detail=f"have not enough rights"
+)
+
+
+user_already_exists_exception = HTTPException(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    detail="User with this email already exists"
+)
+
+
+user_not_found_exception = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail="User not found"
+)
+
+
+update_ban_status_exception = HTTPException(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    detail="Failed to update user's ban status"
 )
