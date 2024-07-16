f26b5e6a0d73866e766f42b38130ad0707847eff
Kirill <pavlenkokirill120@gmail.com>
2024-07-16 17:08:20 +0300

    models

--- booking-system/booking_service/booking/models.py
+++ booking-system/booking_service/booking/models.py
@@ -1,30 +1,29 @@
-from datetime import date, timedelta
+from datetime import date
 from typing import TYPE_CHECKING
 from sqlalchemy import (
-    JSON,
     TIMESTAMP, 
     ForeignKey, 
     MetaData, 
-    UniqueConstraint, 
     func
 )
 from sqlalchemy.orm import (
     Mapped,
     DeclarativeBase,
     mapped_column,
-    declared_attr
+    declared_attr,
+    relationship
 )
 
 from sqlalchemy.ext.hybrid import hybrid_property
 
 from config import settings
 
-if TYPE_CHECKING:
-    from booking.enums import (
+from booking.enums import (
         RoomType, 
         RoomStatus, 
         BookingStatus
     )
+    
 
 
 class Base(DeclarativeBase):
@@ -39,42 +38,58 @@ class Base(DeclarativeBase):
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
     
+    
+class RoomAvailableDate(Base):
+    room_id: Mapped[int] = mapped_column(ForeignKey('room.id'))
+    date: Mapped[date]
+    
+    room = relationship("Room", back_populates="available_dates")
+
+
+class RoomTypeInfo(Base):
+    room_id: Mapped[int] = mapped_column(ForeignKey('room.id'))
+    name = Mapped["RoomType"]
+
+    room = relationship("Room", back_populates="room_types")
+
 
 class Room(Base):
     number: Mapped[int] = mapped_column(unique=True)
-    type: Mapped[list["RoomType"]]
     price: Mapped[int]
     status: Mapped["RoomStatus"]
     description: Mapped[str]
-    available_dates: Mapped[list[date] | None]
     created_at: Mapped[date] = mapped_column(TIMESTAMP, server_default=func.now())
     updated_at: Mapped[date] = mapped_column(TIMESTAMP, server_default=func.now(), onupdate=func.now())
 
+    available_dates = relationship("RoomAvailableDate", back_populates="room")
+    room_types = relationship("RoomTypeInfo", back_populates="room")
+    bookings = relationship("Booking", back_populates="room")
+
 
 class Booking(Base):
-    room_number: Mapped[int] = mapped_column(ForeignKey("room.number"), unique=True)
-    guest_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    room_id: Mapped[int] = mapped_column(ForeignKey("room.id"))
+    guest_id: Mapped[int]
     check_in_date: Mapped[date]
     check_out_date: Mapped[date]
     status: Mapped["BookingStatus"]
-    room: Mapped["Room"]
-    dates: Mapped[list[date]]
+    room = relationship("Room", back_populates="bookings")
+    # dates: Mapped[list[date]]
 
-    @hybrid_property
-    def date_range(self):
-        return [self.check_in_date + timedelta(days=day) for day in range((self.check_out_date - self.check_in_date).days + 1)]
+    # @hybrid_property
+    # def date_range(self):
+    #     return [self.check_in_date + timedelta(days=day) for day in range((self.check_out_date - self.check_in_date).days + 1)]
 
-    @date_range.setter
-    def date_range(self, value):
-        self.dates = value
+    # @date_range.setter
+    # def date_range(self, value):
+    #     self.dates = value
 
-    def __init__(self, guest_id, room_id, check_in_date, check_out_date, status):
-        self.guest_id = guest_id
-        self.room_id = room_id
-        self.check_in_date = check_in_date
-        self.check_out_date = check_out_date
-        self.status = status
-        self.dates = self.date_range
+    # def __init__(self, guest_id, room_id, check_in_date, check_out_date, status):
+    #     self.guest_id = guest_id
+    #     self.room_id = room_id
+    #     self.check_in_date = check_in_date
+    #     self.check_out_date = check_out_date
+    #     self.status = status
+    #     self.dates = self.date_range
 
 
 
